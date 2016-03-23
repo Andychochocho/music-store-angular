36045873567f8883b851625891ee0d87c169509b
Andrew Cho and Tim Durward <timothy.a.durward@gmail.com>
2016-03-23 10:23:36 -0700

    add album components

---
+++ app/album.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Album } from './album.model';
+
+@Component({
+  selector: 'album-display',
+  inputs: ['album'],
+template:`
+  <div>
+  <input *ngIf="album.done" type="checkbox" checked (click)="toggleDone(false)"/>
+  <input *ngIf="!album.done" type="checkbox" (click)="toggleDone(true)"/>
+  <label>{{album.description}}</label>
+`
+})
+
+export class AlbumComponent {
+ public album: Album;
+ toggleDone(setState: boolean){
+ this.album.done = setState;
+ }
